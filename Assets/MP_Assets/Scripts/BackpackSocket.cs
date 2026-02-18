03a10e7a979ab9efd5fd1202c3453136fb22cb8b
Bananapad987 <109549589+Bananapad987@users.noreply.github.com>
2026-02-17 19:23:25 -0600

    Added UI and stuff

--- Assets/MP_Assets/Scripts/BackpackSocket.cs
+++ Assets/MP_Assets/Scripts/BackpackSocket.cs
@@ -1,4 +1,6 @@
 using System.Runtime.InteropServices.WindowsRuntime;
+using TMPro;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Filtering;
@@ -11,16 +13,48 @@ public class BackpackSocket : MonoBehaviour, IXRSelectFilter, IXRHoverFilter
     public bool canProcess => isActiveAndEnabled;
 
     string[] order_tag = {"Pokedex", "Phone", "Berry", "Pokeball"};
+
+
     int idx = 0;
 
     public GameObject door;
+    public GameObject door_wall;
+
+    public GameObject todo_ui;
+
+    TMP_Text todo_list_text;
+
+    string default_text = "\nRemember to put items\nin bag before you go!\n\n";
 
     Animator door_ani;
+
     void Start()
     {
         door_ani = door.GetComponent<Animator>();
+        todo_list_text = todo_ui.GetComponent<TMP_Text>();
+        UpdateText();
     }
 
+    void UpdateText()
+    {
+        string text1 = default_text;
+
+        for (int i = 0; i < order_tag.Length; i++)
+        {
+            text1 += (i+1) + ") " + order_tag[i] + "[";
+            if (idx > i)
+            {
+                text1 += "X";
+            }
+            else
+            {
+                text1 += " ";
+            }
+
+            text1 += "]\n";
+        }
+        todo_list_text.text = text1;
+    }
 
     bool IXRSelectFilter.Process(IXRSelectInteractor interactor, IXRSelectInteractable interactable)
     {
@@ -47,10 +81,12 @@ public class BackpackSocket : MonoBehaviour, IXRSelectFilter, IXRHoverFilter
             Debug.Log("FOUND ITEM: " + order_tag[idx]);
             Destroy(args.interactableObject.transform.gameObject);
             idx++; 
+            UpdateText();
         }
         if (idx >= order_tag.Length)
         {
             door_ani.SetTrigger("Open");
+            door_wall.GetComponent<MeshCollider>().enabled = false;
         }
     }
 }
